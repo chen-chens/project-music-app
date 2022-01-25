6a7bdd11871635abd6d7007f3bb9897e678d2f3e
Joanna.chen <joanna.chen@ewinsonic.com>
2022-01-25 15:41:03 +0800

    [v.0.0.2] - add RWD lauout

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
+import { Button, Typography } from 'antd';
 import React from 'react';
 import './App.css';
+import Home from './home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Home />
   );
 }
 
