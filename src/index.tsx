0fd54922f9a65aa4a4ab3739ff5c948e36488d1d
Joanna.chen <joanna.chen@ewinsonic.com>
2022-02-14 23:28:22 +0800

    [v0.2.14] - Replace BrowserRouter to HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { Provider } from 'react-redux';
 import { store } from './reduxToolkit/store';
-import { ThemeContext, themes } from './theme';
 
 
 ReactDOM.render(
